506f3707d731ebd437716b2d4307d93658c1a966
Hoffman Zheng <hoffman.zheng@goat.com>
2023-04-13 10:41:04 +0800

    backup from old mac

---
+++ variables/target/debug/deps/variables-f171ccd4fb66850b.d
@@ -0,0 +1,5 @@
+/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/deps/variables-f171ccd4fb66850b: src/main.rs
+
+/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/deps/variables-f171ccd4fb66850b.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/variables: /Users/hoffman.zheng/Documents/code_local/rust-projects/variables/src/main.rs
