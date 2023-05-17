500b740ccddb9221ee3a5eaed195273c7f4737ab
Hoffman Zheng <hoffman.zheng@goat.com>
2023-05-17 19:58:34 +0800

    rename directory, rm target

--- variables/target/debug/deps/variables-f171ccd4fb66850b.d
+++
@@ -1,5 +0,0 @@
-/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/deps/variables-f171ccd4fb66850b: src/main.rs
-
-/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/deps/variables-f171ccd4fb66850b.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/hoffman.zheng/Documents/code_local/rust-projects/variables/target/debug/variables: /Users/hoffman.zheng/Documents/code_local/rust-projects/variables/src/main.rs
